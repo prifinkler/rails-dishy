c8dc4fdb9860f98753e2014fee4f8dfdfd9511b2
khadija swaleh <khadija12345@hotmail.co.uk>
2023-09-02 13:57:32 +0200

    created validations and references on models

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,2 +1,6 @@
 class Cuisine < ApplicationRecord
+  has many :users, through: :cuines_users
+  has many :recipes, through: :cuisines_recipes
+
+  validates :name, presence: true, length: { minimum: 2}
 end
--- app/models/dietary.rb
+++ app/models/dietary.rb
@@ -1,2 +1,5 @@
 class Dietary < ApplicationRecord
+  has many :users, :through :dietaries_users
+  has many :recipes, through: :dietaries_recipes
+  validates :dietary_types, presence: true, length: { minimum: 2}
 end
