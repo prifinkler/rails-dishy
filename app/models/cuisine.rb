9d757a6b5e433e7428f8d6ad6ad115f538d8717c
Priscila Finkler Innocente <130703868+prifinkler@users.noreply.github.com>
2023-09-02 14:21:32 +0100

    Update cuisine.rb

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,5 +1,5 @@
 class Cuisine < ApplicationRecord
-  has many :users, through: :cuines_users
+  has many :users, through: :cuisines_users
   has many :recipes, through: :cuisines_recipes
 
   validates :name, presence: true, length: { minimum: 2}
