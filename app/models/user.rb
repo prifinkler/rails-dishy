bf3ff98015c6d9e0583da0cad8b18a9102a092d3
Priscila Finkler Innocente <130703868+prifinkler@users.noreply.github.com>
2023-09-06 21:16:38 +0100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :recipes
+  has_many :favourites
   has_many :recipes, through: :favourites
+  
   has_many :user_cuisines
   has_many :cuisines, through: :user_cuisines
+  
   has_many :user_dietaries
   has_many :dietaries, through: :user_dietaries
+  
   has_many :user_ingredients
   has_many :ingredients, through: :user_ingredients
+  
   devise :database_authenticatable, :registerable,
       :recoverable, :rememberable, :validatable
 end
