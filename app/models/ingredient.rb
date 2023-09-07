ef8214a4ccfee28d0072fc4336bce5243bee1137
Priscila Finkler Innocente <130703868+prifinkler@users.noreply.github.com>
2023-09-07 20:39:18 +0100

    Update ingredient.rb

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ApplicationRecord
-  has_many :user_dietaries
-  has_many :recipe_dietaries
+  has_many :user_ingredients
+  has_many :recipe_ingredients
 
   has_many :users, through: :user_ingredients
   has_many :recipes , through: :recipe_ingredients
