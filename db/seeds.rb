0a3779be36c47e9a2355eac776978b5d58862af8
khadija swaleh <khadija12345@hotmail.co.uk>
2023-09-12 15:31:39 +0200

    added 5 seeds of real recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -147,7 +147,21 @@ ingredients = [
   { name: 'Sugar' },
   { name: 'Nori seaweed' },
   { name: 'Sesame seeds' },
-  { name: 'Pickled ginger' }
+  { name: 'Pickled ginger' },
+  {name: "Chicken thighs"},
+  {name: "Mint leaves"},
+  {name: "ROSE HARISSA"},
+  { name: 'Garlic cloves' },
+  { name: 'Tomato' },
+  { name: 'Flat-leaf parsley' },
+  { name: 'Ground coffee' },
+  { name: 'Cascabel chilli' },
+  { name: 'Soft light brown sugar' },
+  { name: 'Smoked paprika' },
+  { name: 'Ground cumin' },
+  { name: 'Aubergines' },
+  { name: 'Black garlic cloves' },
+  { name: 'Greek-style yoghurt' }
 ]
 
 ingredients.each do |ingredient|
@@ -468,6 +482,181 @@ Recipe.last.cuisines << Cuisine.find_by(name: "Japanese")
 Recipe.last.dietaries << Dietary.find_by(dietary_type: "Vegetarian")
 
 puts "10 recipes created!"
+
+aubergine_with_black_garlic_recipe = Recipe.create!(
+  name: 'Slices of Roast Aubergine with Black Garlic Sauce',
+  description: 'A delicious dish featuring roasted aubergine and a flavorful black garlic sauce.',
+  time: 40,
+  photo_url: 'seed/roasted_aubergine.jpg',
+  instruction: '1. Preheat the oven to 250°C/480°F/gas 9 (or its highest setting).
+                2. In a large bowl, mix aubergine rounds with 60ml of olive oil, 1½ tsp salt, and black pepper.
+                3. Spread the aubergine slices on two large baking trays lined with greaseproof paper.
+                4. Roast in the oven until golden-brown and completely soft, about 30 minutes.
+                5. Remove from the oven and let them cool.
+                6. In a small food processor, blend the BLACK GARLIC CLOVES with 1/3 tsp salt, 2 tbsp olive oil, 2 tbsp yogurt, and lemon juice to form a rough paste.
+                7. Transfer the paste to a medium bowl and mix in the remaining yogurt. Chill in the fridge.
+                8. Heat the remaining 110ml of oil in a small saucepan over high heat.
+                9. Add sliced garlic and chilli and fry on medium heat for about 5 minutes until golden-brown and crispy.
+                10. Use a slotted spoon to transfer the garlic and chilli to a paper-lined plate.
+                11. Arrange the roasted aubergine slices on a platter.
+                12. Spoon the black garlic sauce over the aubergine.
+                13. Sprinkle the crispy garlic and chilli over the top.
+                14. Finish by garnishing with dill, basil, and tarragon leaves.
+                15. Season with salt and black pepper to taste.
+                16. Serve and enjoy!'
+)
+
+aubergine_with_black_garlic_recipe.ingredients << Ingredient.find_or_create_by(name: "Aubergines")
+aubergine_with_black_garlic_recipe.ingredients << Ingredient.find_or_create_by(name: "Olive oil")
+aubergine_with_black_garlic_recipe.ingredients << Ingredient.find_or_create_by(name: "Black garlic cloves")
+aubergine_with_black_garlic_recipe.ingredients << Ingredient.find_or_create_by(name: "Greek yoghurt")
+aubergine_with_black_garlic_recipe.ingredients << Ingredient.find_or_create_by(name: "Lemon juice")
+aubergine_with_black_garlic_recipe.ingredients << Ingredient.find_or_create_by(name: "Garlic cloves")
+aubergine_with_black_garlic_recipe.ingredients << Ingredient.find_or_create_by(name: "Red chillies")
+aubergine_with_black_garlic_recipe.ingredients << Ingredient.find_or_create_by(name: "Dill leaves")
+aubergine_with_black_garlic_recipe.ingredients << Ingredient.find_or_create_by(name: "Basil")
+aubergine_with_black_garlic_recipe.ingredients << Ingredient.find_or_create_by(name: "Tarragon leaves")
+
+Recipe.last.cuisines << Cuisine.find_or_create_by(name: "Mediterranean")
+Recipe.last.dietaries << Dietary.find_or_create_by(dietary_type: "Vegetarian")
+
+pappardelle_recipe = Recipe.create!(
+  name: 'Pappardelle with Harissa, Cherry Tomatoes, and Olives',
+  description: 'A flavorful pasta dish with a spicy kick, perfect for pasta lovers.',
+  time: 40,
+  photo_url: 'seed/pappardelle_harissa.jpg',
+  instruction: '1. Heat olive oil in a large sauté pan with a lid over medium-high heat.
+                2. Add thinly sliced onion and fry for 8 minutes, stirring occasionally, until soft and caramelized.
+                3. Stir in ROSE HARISSA, cherry tomatoes, torn Kalamata olives, capers, and ½ teaspoon of salt.
+                4. Continue to fry for 3–4 minutes, stirring frequently, until the tomatoes start to break down.
+                5. Add 200ml of water and stir. Bring to a boil, then reduce the heat to medium-low, cover, and simmer for 10 minutes.
+                6. Remove the lid and continue to cook for 4–5 minutes, until the sauce is thick and rich. Stir in 10g of parsley and set aside.
+                7. In a large pot, boil salted water and cook the pappardelle according to the packet instructions until al dente. Drain well.
+                8. Return the pasta to the pot and add harissa sauce and 1/8 teaspoon of salt. Mix together.
+                9. Divide the pasta among four shallow bowls.
+                10. Serve hot with a spoonful of Greek-style yoghurt and a final sprinkle of parsley.'
+)
+
+pappardelle_recipe.ingredients << Ingredient.find_or_create_by(name: "Olive oil")
+pappardelle_recipe.ingredients << Ingredient.find_or_create_by(name: "Onion")
+pappardelle_recipe.ingredients << Ingredient.find_or_create_by(name: "ROSE HARISSA")
+pappardelle_recipe.ingredients << Ingredient.find_or_create_by(name: "Cherry tomatoes")
+pappardelle_recipe.ingredients << Ingredient.find_or_create_by(name: "Kalamata olives")
+pappardelle_recipe.ingredients << Ingredient.find_or_create_by(name: "Capers")
+pappardelle_recipe.ingredients << Ingredient.find_or_create_by(name: "Pappardelle pasta")
+pappardelle_recipe.ingredients << Ingredient.find_or_create_by(name: "Parsley")
+pappardelle_recipe.ingredients << Ingredient.find_or_create_by(name: "Greek-style yoghurt")
+pappardelle_recipe.ingredients << Ingredient.find_or_create_by(name: "Salt")
+
+Recipe.last.cuisines << Cuisine.find_or_create_by(name: "Italian")
+Recipe.last.dietaries << Dietary.find_or_create_by(dietary_type: "Vegetarian")
+
+koftas_recipe = Recipe.create!(
+  name: 'Coffee-Spiced Chicken Koftas',
+  description: 'Flavorful chicken koftas with a smoky coffee rub and grilled onions.',
+  time: 80,
+  photo_url: 'seed/coffee_koftas.jpg',
+  instruction: '1. Put the chicken thighs in a food processor and pulse until minced.
+                2. Transfer the minced chicken to a large bowl and add grated onion, crushed garlic, grated tomato (skin discarded), chopped parsley, lemon zest, 1 tbsp olive oil, 3/4 tsp salt, and a good grind of pepper. Knead the mixture for about three minutes until well combined and sticky. Divide into 12 portions and form them into compact, torpedo-shaped koftas of about 55g each.
+                3. Make the rub by combining finely ground coffee, ground cascabel chilli, soft light brown sugar, smoked paprika, and ground cumin with 1/2 tsp salt. Set aside one-third of the rub mixture and spread the rest over a baking tray. Roll each kofta in the coffee rub until well coated and set aside (or refrigerate if preparing ahead).
+                4. Preheat the oven to 240°C (220°C fan)/475°F/gas 9. Toss the red onion rounds with 1 tbsp oil, 1/4 tsp salt, and pepper. Grill the onion slices in a well-greased griddle pan on a high heat for four minutes on each side until well charred and softened, then set aside to cool.
+                5. Drizzle the koftas with 1 tbsp oil and grill them in two batches for about 90 seconds per batch until lightly charred on the outsides. Transfer the grilled kofta to a medium oven tray lined with greaseproof paper and roast in the hot oven for six minutes. Combine the reserved coffee rub with two tablespoons of oil and drizzle it all over the kofta. Return to the oven for another three minutes until cooked through and nicely browned.
+                6. In a bowl, toss the grilled onion with mint, picked parsley, lemon juice, and a pinch of salt and pepper.
+                7. Arrange the koftas and their cooking juices on a platter and pile the grilled onion salad to one side. Squeeze the lemon wedges over the koftas and serve warm.'
+)
+
+koftas_recipe.ingredients << Ingredient.find_or_create_by(name: "Chicken thighs")
+koftas_recipe.ingredients << Ingredient.find_or_create_by(name: "Onion")
+koftas_recipe.ingredients << Ingredient.find_or_create_by(name: "Garlic cloves")
+koftas_recipe.ingredients << Ingredient.find_or_create_by(name: "Tomato")
+koftas_recipe.ingredients << Ingredient.find_or_create_by(name: "Flat-leaf parsley")
+koftas_recipe.ingredients << Ingredient.find_or_create_by(name: "Lemon")
+koftas_recipe.ingredients << Ingredient.find_or_create_by(name: "Olive oil")
+koftas_recipe.ingredients << Ingredient.find_or_create_by(name: "Salt")
+koftas_recipe.ingredients << Ingredient.find_or_create_by(name: "Black pepper")
+koftas_recipe.ingredients << Ingredient.find_or_create_by(name: "Red onions")
+koftas_recipe.ingredients << Ingredient.find_or_create_by(name: "Mint leaves")
+koftas_recipe.ingredients << Ingredient.find_or_create_by(name: "Ground coffee")
+koftas_recipe.ingredients << Ingredient.find_or_create_by(name: "Ground cascabel chilli")
+koftas_recipe.ingredients << Ingredient.find_or_create_by(name: "Soft light brown sugar")
+koftas_recipe.ingredients << Ingredient.find_or_create_by(name: "Smoked paprika")
+koftas_recipe.ingredients << Ingredient.find_or_create_by(name: "Ground cumin")
+
+
+Recipe.last.cuisines << Cuisine.find_or_create_by(name: "Greek")
+Recipe.last.dietaries << Dietary.find_or_create_by(dietary_type: "Poultry")
+
+jollof_rice_recipe = Recipe.create!(
+  name: 'Jollof Rice',
+  description: 'Jollof rice gets a smoky flavor boost in this delicious jollof recipe from Lerato Umah-Shaylor.',
+  time: 60,
+  photo_url: 'seed/jollof_rice.jpg', # Add the URL of the recipe photo
+  instruction: '1. Put all the puree ingredients in a food processor, except the scotch bonnets, if using whole, and whizz into a thick and aromatic puree.
+                2. Put the oil in a wide large saucepan, for which you have a tightly fitting lid, and set over a medium heat. Add the red onion and a pinch of fine sea salt. Cook for 15 minutes, stirring often, until softened and golden. If it starts to get dry, add a little splash of water to prevent the onion from burning.
+                3. Stir in the tomato puree and cook for 2-3 minutes, until the puree starts to separate. Add the spices and herbs and cook for 2 minutes, stirring continuously.
+                4. Gently pour in the blended puree, stirring well, then cover and cook for up to 25 minutes, until the puree is reduced to a drier sauce. Keep a close eye on it and stir occasionally.
+                5. Add the rice and stock (about 250ml for basmati rice and up to 400ml for long-grain rice), ensuring there is enough water to just submerge the rice. Season with the fine sea salt and stir just once. Add the scotch bonnets, if using whole. Bring to a boil, then reduce the heat, cover, and simmer over a low-medium heat for up to 30 minutes. The sauce must be visibly simmering to ensure the rice cooks properly.
+                6. Once the rice is cooked, remove the pan from the heat and leave covered to steam for a few minutes. Fluff with a fork, scatter over the reserved thyme leaves, and enjoy your wonderful creation with a fresh salad and/or fried plantains, because jollof and plantains are a match made in heaven.'
+)
+
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Long-grain or basmati rice")
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Vegetable or chicken stock")
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Fine sea salt")
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Plantains")
+
+# For the puree
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Vine-ripened tomatoes or tin plum tomatoes")
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Romano or red peppers")
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Large onion")
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Piece of ginger")
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Garlic cloves")
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Red or yellow scotch bonnet peppers")
+
+# For the jollof base
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Coconut, rapeseed, or vegetable oil")
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Small red onion")
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Tomato puree")
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Curry powder")
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Smoked paprika")
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Bay leaf")
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Dried thyme")
+jollof_rice_recipe.ingredients << Ingredient.find_or_create_by(name: "Thyme sprigs")
+
+
+Recipe.last.dietaries << Dietary.find_or_create_by(dietary_type: "Dairy-Free")
+Recipe.last.dietaries << Dietary.find_or_create_by(dietary_type: "Gluten-Free")
+Recipe.last.dietaries << Dietary.find_or_create_by(dietary_type: "Vegan")
+Recipe.last.cuisines << Cuisine.find_or_create_by(name: "African")
+
+scully_pork_belly = Recipe.create!(
+  name: "Scully's crispy pork belly",
+  description: 'Experience the perfection of Scullys crispy and flavorful pork belly with seasonal relishes.',
+  time: 180,
+  photo_url: 'seed/pork_belly.jpg',
+  instruction: "
+    1. Heat the oven to 250°C/Gas Mark 10 or its highest setting.
+    2. Place the herbs, garlic, and olive oil in a heavy-duty blender or food processor and roughly purée them.
+    3. Lay the pork belly in an oven tray, skin-side down, and lightly sprinkle with salt and pepper. Spread the herb mixture evenly over the top, pressing it onto the meat.
+    4. Turn the belly skin-side up, dry the skin with kitchen paper, and sprinkle sea salt evenly over the skin (avoid using too much). Place the tray in the oven and roast for 1 hour, turning it every 20 minutes.
+    5. Once the skin starts forming crackling, reduce the oven temperature to 170°C/Gas Mark 3, pour the white wine into the tray (avoiding the pork skin), and continue roasting for another hour. If the belly starts turning black, cover it with foil.
+    6. For the final stage, reduce the oven temperature to 110°C/Gas Mark ¼ and continue roasting for another hour, until the skin has completely crackled and dried.
+    7. Remove the pork from the oven. Use a sharp knife to divide it into segments of a few ribs, cutting between the rib bones. Serve with seasonal relishes on the side."
+)
+
+# Create ingredients
+scully_pork_belly.ingredients << Ingredient.find_or_create_by(name: "1 bunch of thyme, roughly chopped")
+scully_pork_belly.ingredients << Ingredient.find_or_create_by(name: "1 bunch of rosemary, roughly chopped")
+scully_pork_belly.ingredients << Ingredient.find_or_create_by(name: "1 head of garlic, cloves peeled and crushed")
+scully_pork_belly.ingredients << Ingredient.find_or_create_by(name: "150ml olive oil")
+scully_pork_belly.ingredients << Ingredient.find_or_create_by(name: "1 piece of pork belly, weighing 2-3kg")
+scully_pork_belly.ingredients << Ingredient.find_or_create_by(name: "1 bottle of white wine")
+scully_pork_belly.ingredients << Ingredient.find_or_create_by(name: "Coarse sea salt and black pepper")
+
+# Create cuisines and dietaries
+scully_pork_belly.cuisines << Cuisine.find_or_create_by(name: "American") # You can change this to the appropriate cuisine
+scully_pork_belly.dietaries << Dietary.find_or_create_by(dietary_type: "High-protein")
+
+puts "5 more recipes added"
 
 User.create!(
   email: 'user@gmail.com',
