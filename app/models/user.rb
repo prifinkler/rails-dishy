403e882e1214563da7ae0c9c3bee7d29ee192ddf
rachelholden <rachelbholden@gmail.com>
2023-09-12 20:34:42 +0100

    adding destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :favourites
+  has_many :favourites, dependent: :destroy
   has_many :recipes, through: :favourites, source: :recipe
 
-  has_many :user_cuisines
+  has_many :user_cuisines, dependent: :destroy
   has_many :cuisines, through: :user_cuisines
 
-  has_many :user_dietaries
+  has_many :user_dietaries, dependent: :destroy
   has_many :dietaries, through: :user_dietaries
 
-  has_many :user_ingredients
+  has_many :user_ingredients, dependent: :destroy
   has_many :ingredients, through: :user_ingredients
 
   devise :database_authenticatable, :registerable,
