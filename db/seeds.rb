8ce35a50eaf4eff1cc8bff3bda741ab2adba1a5c
khadija swaleh <khadija12345@hotmail.co.uk>
2023-09-28 21:39:24 +0200

    none and all removed from filter

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,6 @@ Ingredient.destroy_all
 puts "Database cleaned"
 
 dietaries = [
-  { dietary_type: 'None' },
   { dietary_type: 'Vegan' },
   { dietary_type: 'Vegetarian' },
   { dietary_type: 'Pescatarian' },
@@ -51,7 +50,6 @@ end
 puts "Dietary types created!"
 
 cuisines = [
-  { name: 'All' },
   { name: 'African' },
   { name: 'American' },
   { name: 'Brazilian' },
