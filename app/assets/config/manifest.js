8c51bb8e56dc4d1a27bf2da9aab20a5cb86426de
Priscila Finkler <prifinkler@gmail.com>
2024-11-05 00:51:05 +0000

    updated manifest.js for production

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,10 @@
+// app/assets/config/manifest.js
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_tree ../builds
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/swiper_controller.js
+//= link controllers/favorites_controller.js
