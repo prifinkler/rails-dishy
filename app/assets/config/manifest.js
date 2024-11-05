7383720af53623bb439b0440bee9ccd1614e5e9e
Priscila Finkler <prifinkler@gmail.com>
2024-11-05 01:45:27 +0000

    manifest updated paths

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,5 @@
-// app/assets/config/manifest.js
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link controllers/application.js
-//= link controllers/hello_controller.js
-//= link controllers/swiper_controller.js
-//= link controllers/favorites_controller.js
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+
